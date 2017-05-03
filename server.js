6bea0ac452278737990ca0b8a5abdf0a149d6d65
System <none>
2017-05-03 07:48:59 +0000

    done

--- server.js
+++ server.js
@@ -20,8 +20,11 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  var ip = req.ip.slice(7);
+  var languages = req.get('accept-language');
+  var user = req.get('user-agent');
+  res.json({ipaddress: ip, language: languages, software: user});
 });
 
 
